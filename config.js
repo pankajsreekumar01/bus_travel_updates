3254cd741a0153ee231f8453064d15778bc802aa
pankaj sreekumar <pankaj.pro.x@gmail.com>
2026-03-10 01:59:05 +0530

    Update webhook URL

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 // Replace the URL below with your Webhook.site URL or any other webhook URL.
 // IMPORTANT: Make sure the URL is enclosed in single quotes (' ') or double quotes (" ").
 
-const WEBHOOK_URL = 'https://webhook.site/6ae0f3a7-9305-45c1-a40c-44c64694f48b';
+const WEBHOOK_URL = 'https://webhook.site/8517cea1-5220-4e4e-a4a6-5cf01732b7c7';
