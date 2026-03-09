16550ee368e6f1ca6b05508234863422ebbfcccd
pankaj sreekumar <pankaj.pro.x@gmail.com>
2026-03-09 23:46:25 +0530

    Fix map visibility and extract webhook url to config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// ==========================================
+// CONFIGURATION FILE
+// ==========================================
+
+// Replace the URL below with your Webhook.site URL or any other webhook URL.
+// IMPORTANT: Make sure the URL is enclosed in single quotes (' ') or double quotes (" ").
+
+const WEBHOOK_URL = 'https://webhook.site/0e622ff9-fb00-4af3-b099-5804a0c393f7';
