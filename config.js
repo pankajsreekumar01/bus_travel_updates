44a6585c3f36170447ea8ba5788dd213205dd245
Herbiosta <82394837+pankajsreekumar01@users.noreply.github.com>
2026-03-23 14:23:29 +0530

    Update config.js
    23 march webook

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 // Replace the URL below with your Webhook.site URL or any other webhook URL.
 // IMPORTANT: Make sure the URL is enclosed in single quotes (' ') or double quotes (" ").
 
-const WEBHOOK_URL = 'https://webhook.site/8517cea1-5220-4e4e-a4a6-5cf01732b7c7';
+const WEBHOOK_URL = 'https://webhook.site/028209ae-afab-4757-9459-48d7423a1d49';
