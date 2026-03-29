d0fcefd2d57cc0312e0a93579589b200366d7792
Herbiosta <82394837+pankajsreekumar01@users.noreply.github.com>
2026-03-29 16:39:34 +0530

    Updated config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 // Replace the URL below with your Webhook.site URL or any other webhook URL.
 // IMPORTANT: Make sure the URL is enclosed in single quotes (' ') or double quotes (" ").
 
-const WEBHOOK_URL = 'https://webhook.site/028209ae-afab-4757-9459-48d7423a1d49';
+const WEBHOOK_URL = 'https://webhook.site/d4c56e99-304a-46b2-8de9-c73302e98df8';
